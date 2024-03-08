b72e72f53be055a8977442b8630f4f56e45b476c
Andy Jones <andy.jones@digital.education.gov.uk>
2024-03-08 19:14:23 +0000

    escape fix

--- app/assets/js/feedback.js
+++ app/assets/js/feedback.js
@@ -18,6 +18,21 @@ servicesTab.addEventListener('click', function (event) {
   }
 });
 
+document.addEventListener('keydown', function (event) {
+  // Check if the Escape key is pressed
+  if (event.key === "Escape" || event.key === "Esc") {
+    // Prevent the default action to avoid any side effects
+    event.preventDefault();
+
+    // Check if the servicesPanel is currently displayed
+    if (servicesPanel.style.display === 'block') {
+      servicesPanel.style.display = 'none';
+      servicesPanel.setAttribute('aria-hidden', 'true');
+      servicesTab.setAttribute('aria-expanded', 'false');
+    }
+  }
+});
+
 closeNav.addEventListener('click', function (event) {
   event.preventDefault();
 
