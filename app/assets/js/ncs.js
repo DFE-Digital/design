6769149d882cefdb17f03dc4187366c31aef209d
Andy Jones <andy.jones@digital.education.gov.uk>
2025-03-01 23:36:52 +0000

    New manual

--- app/assets/js/ncs.js
+++
@@ -1,7 +0,0 @@
-// initialize the iFrame resizer
-var iframe = document.querySelectorAll('[data-module="app-iframe"]');
-for(let i=0; i<iframe.length; i++){
-    iframe[i].onload = function() {
-    iframe[i].style.height = 20 + iframe[i].contentWindow.document.body.scrollHeight + 'px';
-    }
-}
